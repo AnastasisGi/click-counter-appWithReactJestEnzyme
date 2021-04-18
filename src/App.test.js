b9fd8fdb76c3e2640081b5c09de5e70a8487b596
AnastasisGi <anastasiosgks@gmail.com>
2021-04-18 18:27:39 +0100

    corrected the enzyme adaptor

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,13 @@
-import Enzyme,{EnzymeAdapter, shallow} from 'enzyme';
+import React from 'react'
+import Enzyme, {shallow} from 'enzyme';
 import EnzymeΑdapter from '@wojtekmaj/enzyme-adapter-react-17'
 import App from './App';
 
-Enzyme.configure({adapter:new EnzymeAdapter()})
+Enzyme.configure({adapter:new EnzymeΑdapter()})
 
 
 test('renders without error', () => {
-
-  
+const wrapper = shallow(<App />)
 });
 
 
